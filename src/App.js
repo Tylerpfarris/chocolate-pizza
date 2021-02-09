034aa2e0f435a2517da977572c0341f22732ec03
Tyler Farris <Tylerpfarris@gmail.com>
2021-02-08 16:05:10 -0800

    things are rendering

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import ChocolatePizzaRecipe from './ChocolatePizzaRecipe.js';
 import IngredientList from './IngredientList.js';
 import PizzaChocolateFooter from './PizzaChocolateFooter.js';
 
+import Style from './app.module.css';
+
 import pizzaImg from './lab-assets/choco-pizza.png';
 import fbIcon from './lab-assets/fb-icon.png';
 import flicIcon from './lab-assets/flic-icon.png';
 import gpIcon from './lab-assets/gp-icon.png';
 import hrImg from './lab-assets/hr-img.png';
-import instIcon from './lab-assets/hr-img.png';
+import instIcon from './lab-assets/insta-icon.png';
 
 
 
@@ -22,7 +24,8 @@ export default class ChocolatePizzaPage extends React.Component {
           button2={flicIcon}
           button3={gpIcon}
           button4={instIcon} />
-        <img src="./lab-assets/choco-pizza.png" alt="choco pizza" />
+          <h1>Chocolate Pizza</h1>
+        <img src={pizzaImg} alt="choco pizza" />
         <ChocolatePizzaRecipe />
         <IngredientList />
         <PizzaChocolateFooter />
--- src/IngredientList.js
+++ src/IngredientList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ingredients } from './data.js';
 
+
+
 export default class IngredientList extends React.Component {
     render() {
 
         const ingredientItems = ingredients.map(ingredient => <div>
-            <span>{this.props.ingredient.name}</span>
-            <span>{this.props.ingredient.amount}</span>
+            <span>{ingredient.name} {ingredient.amount}</span>
+           
         </div>)
         return (
             <div>{ingredientItems}</div>
